835ac6c80f1b835d9c218dd276831a8cf5d05a0e
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-07 00:07:34 +0530

    Shipping Charges

--- backend/controllers/paymentControllers.jsx
+++ backend/controllers/paymentControllers.jsx
@@ -4,6 +4,8 @@ import Stripe from "stripe";
 const stripe=Stripe(process.env.STRIPE_SECRET_KEY)
 export const stripeCheckoutSession=catchAsyncErrors(
     async(req,res,next)=>{
+        const body=req?.body
+        const shipping_rate=body?.itemsPrice > 200 ? "shr_1PZdS6SFrwBJ9KLtXJC6DNCM" :  "shr_1PZdSlSFrwBJ9KLtYTLNN6Zf"
 
         const session=await stripe.checkout.session.create({
             payment_method_types:['card'],
@@ -11,6 +13,7 @@ export const stripeCheckoutSession=catchAsyncErrors(
             cancel_url:`${process.env.FRONTEND_URL}`,
             customer_email:req?.user?.email,
             client_reference_id:req?.user?._id?.toString(),
-            mode:'payment'
+            mode:'payment',
+            shipping_options:[]
         });
 })
