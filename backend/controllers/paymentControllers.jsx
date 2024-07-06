85775060f9a5afe4c804612c5e6b3edbdcc49fad
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-06 23:47:07 +0530

    starting with payment controller

--- backend/controllers/paymentControllers.jsx
+++ backend/controllers/paymentControllers.jsx
@@ -1,1 +1,5 @@
 // Creating the stripe checkout session => /api/v1/payment/checkout_session
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+export const stripeCheckoutSession=catchAsyncErrors(async(req,res,next)=>{
+
+})
