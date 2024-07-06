228449ad7159986d94308a92649919ed0a6ddfa3
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-06 23:56:05 +0530

    starting with payment controller

--- backend/controllers/paymentControllers.jsx
+++ backend/controllers/paymentControllers.jsx
@@ -9,5 +9,8 @@ export const stripeCheckoutSession=catchAsyncErrors(
             payment_method_types:['card'],
             success_url:`${process.env.FRONTEND_URL}/me/orders`,
             cancel_url:`${process.env.FRONTEND_URL}`,
-        })
+            customer_email:req?.user?.email,
+            client_reference_id:req?.user?._id?.toString(),
+            mode:'payment'
+        });
 })
