aa4e40a6d7da87aac9e04fa2f3823161bb0d5dbc
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-10 23:44:36 +0530

    Authentication check for review

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -4,16 +4,17 @@ import { useGetProductDetailsQuery } from "../../redux/api/productsApi";
 import { toast } from "react-hot-toast";
 import Loader from "../layout/Loader";
 import StarRatings from "react-star-ratings";
-import { useDispatch } from "react-redux";
-import {setCartItem} from "../../redux/features/cartSlice"
+import { useDispatch, useSelector } from "react-redux";
+import { setCartItem } from "../../redux/features/cartSlice"
 import MetaData from "../layout/MetaData";
+import NewReview from "../reviews/NewReview";
 
 
 const ProductDetails = () => {
     const params = useParams();
-    const dispatch=useDispatch();
+    const dispatch = useDispatch();
 
-    const [quantity,setQuantity]=useState(1)
+    const [quantity, setQuantity] = useState(1)
     const [activeImg, setActiveImg] = useState("");
 
     const { data, isLoading, error, isError } = useGetProductDetailsQuery(
@@ -21,7 +22,9 @@ const ProductDetails = () => {
     );
     const product = data?.product;
 
-    
+    const { isAuthenticated } = useSelector((state) => state.auth)
+
+
 
     useEffect(() => {
         setActiveImg(
@@ -37,33 +40,33 @@ const ProductDetails = () => {
         }
     }, [isError]);
 
-    const increaseQty=()=>{
-        const count=document.querySelector(".count")
+    const increaseQty = () => {
+        const count = document.querySelector(".count")
 
-        if(count.valueAsNumber >= product?.stock){
+        if (count.valueAsNumber >= product?.stock) {
             return;
         }
-        const qty=count.valueAsNumber + 1;
+        const qty = count.valueAsNumber + 1;
         setQuantity(qty)
     }
 
-    const decreaseQty=()=>{
-        const count=document.querySelector(".count")
+    const decreaseQty = () => {
+        const count = document.querySelector(".count")
 
-        if(count.valueAsNumber <= product.stock){
+        if (count.valueAsNumber <= product.stock) {
             return;
         }
-        const qty=count.valueAsNumber - 1;
+        const qty = count.valueAsNumber - 1;
         setQuantity(qty)
     }
 
-    const setItemToCart=()=>{
-        const cartItem={
-            product:product?._id,
-            name:product?.name,
-            price:product?.price,
-            image:product?.images[0]?.url,
-            stock:product?.stock,
+    const setItemToCart = () => {
+        const cartItem = {
+            product: product?._id,
+            name: product?.name,
+            price: product?.price,
+            image: product?.images[0]?.url,
+            stock: product?.stock,
             quantity,
 
         };
@@ -75,106 +78,110 @@ const ProductDetails = () => {
 
     return (
         <>
-    <MetaData title={product?.name}/>
-        <div className="row d-flex justify-content-around">
-            <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                <div className="p-3">
-                    <img
-                        className="d-block w-100"
-                        src={activeImg}
-                        alt={product?.name}
-                        width="340"
-                        height="390"
-                    />
+            <MetaData title={product?.name} />
+            <div className="row d-flex justify-content-around">
+                <div className="col-12 col-lg-5 img-fluid" id="product_image">
+                    <div className="p-3">
+                        <img
+                            className="d-block w-100"
+                            src={activeImg}
+                            alt={product?.name}
+                            width="340"
+                            height="390"
+                        />
+                    </div>
+                    <div className="row justify-content-start mt-5">
+                        {product?.images?.map((img) => (
+                            <div className="col-2 ms-4 mt-2">
+                                <a role="button">
+                                    <img
+                                        className={`d-block border rounded p-3 cursor-pointer ${img.url === activeImg ? "border-warning" : ""
+                                            } `}
+                                        height="100"
+                                        width="100"
+                                        src={img?.url}
+                                        alt={img?.url}
+                                        onClick={(e) => setActiveImg(img.url)}
+                                    />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className="row justify-content-start mt-5">
-                    {product?.images?.map((img) => (
-                        <div className="col-2 ms-4 mt-2">
-                            <a role="button">
-                                <img
-                                    className={`d-block border rounded p-3 cursor-pointer ${img.url === activeImg ? "border-warning" : ""
-                                        } `}
-                                    height="100"
-                                    width="100"
-                                    src={img?.url}
-                                    alt={img?.url}
-                                    onClick={(e) => setActiveImg(img.url)}
-                                />
-                            </a>
-                        </div>
-                    ))}
-                </div>
-            </div>
 
-            <div className="col-12 col-lg-5 mt-5">
-                <h3>{product?.name}</h3>
-                <p id="product_id">Product # {product?._id}</p>
+                <div className="col-12 col-lg-5 mt-5">
+                    <h3>{product?.name}</h3>
+                    <p id="product_id">Product # {product?._id}</p>
 
-                <hr />
+                    <hr />
 
-                <div className="d-flex">
-                    <StarRatings
-                        rating={product?.ratings}
-                        starRatedColor="#ffb829"
-                        numberOfStars={5}
-                        name="rating"
-                        starDimension="24px"
-                        starSpacing="1px"
-                    />
-                    <span id="no-of-reviews" className="pt-1 ps-2">
-                        {" "}
-                        ({product?.numOfReviews} Reviews){" "}
-                    </span>
-                </div>
-                <hr />
+                    <div className="d-flex">
+                        <StarRatings
+                            rating={product?.ratings}
+                            starRatedColor="#ffb829"
+                            numberOfStars={5}
+                            name="rating"
+                            starDimension="24px"
+                            starSpacing="1px"
+                        />
+                        <span id="no-of-reviews" className="pt-1 ps-2">
+                            {" "}
+                            ({product?.numOfReviews} Reviews){" "}
+                        </span>
+                    </div>
+                    <hr />
 
-                <p id="product_price">${product?.price}</p>
-                <div className="stockCounter d-inline">
-                    <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
-                    <input
-                        type="number"
-                        className="form-control count d-inline"
-                        value={quantity}
-                        readOnly
-                    />
-                    <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
-                </div>
-                <button
-                    type="button"
-                    id="cart_btn"
-                    className="btn btn-primary d-inline ms-4"
-                    disabled={product.stock<=0}
-                    onClick={setItemToCart}
-                >
-                    Add to Cart
-                </button>
-
-                <hr />
-
-                <p>
-                    Status:{" "}
-                    <span
-                        id="stock_status"
-                        className={product?.stock > 0 ? "greenColor" : "redColor"}
+                    <p id="product_price">${product?.price}</p>
+                    <div className="stockCounter d-inline">
+                        <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
+                        <input
+                            type="number"
+                            className="form-control count d-inline"
+                            value={quantity}
+                            readOnly
+                        />
+                        <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
+                    </div>
+                    <button
+                        type="button"
+                        id="cart_btn"
+                        className="btn btn-primary d-inline ms-4"
+                        disabled={product.stock <= 0}
+                        onClick={setItemToCart}
                     >
-                        {product?.stock > 0 ? "In Stock" : "Out of Stock"}
-                    </span>
-                </p>
+                        Add to Cart
+                    </button>
 
-                <hr />
+                    <hr />
 
-                <h4 className="mt-2">Description:</h4>
-                <p>{product?.description}</p>
-                <hr />
-                <p id="product_seller mb-3">
-                    Sold by: <strong>{product?.seller}</strong>
-                </p>
+                    <p>
+                        Status:{" "}
+                        <span
+                            id="stock_status"
+                            className={product?.stock > 0 ? "greenColor" : "redColor"}
+                        >
+                            {product?.stock > 0 ? "In Stock" : "Out of Stock"}
+                        </span>
+                    </p>
 
-                <div className="alert alert-danger my-5" type="alert">
-                    Login to post your review.
+                    <hr />
+
+                    <h4 className="mt-2">Description:</h4>
+                    <p>{product?.description}</p>
+                    <hr />
+                    <p id="product_seller mb-3">
+                        Sold by: <strong>{product?.seller}</strong>
+                    </p>
+
+                    {isAuthenticated ? (<NewReview />) : (
+                        <div className="alert alert-danger my-5" type="alert">
+                            Login to post your review.
+                        </div>
+                    )}
+
+
                 </div>
             </div>
-        </div>
         </>
     );
 };
