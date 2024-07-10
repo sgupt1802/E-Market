20018ce400a97666f9c5451186289881e41e70b5
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-10 23:58:41 +0530

    Authentication check for review

--- frontend/src/components/reviews/NewReview.jsx
+++ frontend/src/components/reviews/NewReview.jsx
@@ -1,76 +1,85 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import StarRatings from "react-star-ratings";
 const NewReview = () => {
-  return (
-    <>
-    <div>
-      <button
-        id="review_btn"
-        type="button"
-        className="btn btn-primary mt-4"
-        data-bs-toggle="modal"
-        data-bs-target="#ratingModal"
-      >
-        Submit Your Review
-      </button>
+    const [rating,setRating]=useState(0)
+    const [comment,setComment]=useState("")
 
-      <div className="row mt-2 mb-5">
-        <div className="rating w-50">
-          <div
-            className="modal fade"
-            id="ratingModal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="ratingModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="ratingModalLabel">
-                    Submit Review
-                  </h5>
-                  <button
+    const submitHandler=()=>{
+
+    }
+    return (
+        <>
+            <div>
+                <button
+                    id="review_btn"
                     type="button"
-                    className="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  <div className="star-ratings">
-                    <i className="fa fa-star star-active"></i>
-                    <i className="fa fa-star star-active"></i>
-                    <i className="fa fa-star star-active"></i>
-                    <i className="fa fa-star star-active"></i>
-                    <i className="fa fa-star star-active"></i>
-                  </div>
+                    className="btn btn-primary mt-4"
+                    data-bs-toggle="modal"
+                    data-bs-target="#ratingModal"
+                >
+                    Submit Your Review
+                </button>
 
-                  <textarea
-                    name="review"
-                    id="review"
-                    className="form-control mt-4"
-                    placeholder="Enter your comment"
-                  ></textarea>
+                <div className="row mt-2 mb-5">
+                    <div className="rating w-50">
+                        <div
+                            className="modal fade"
+                            id="ratingModal"
+                            tabindex="-1"
+                            role="dialog"
+                            aria-labelledby="ratingModalLabel"
+                            aria-hidden="true"
+                        >
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="ratingModalLabel">
+                                            Submit Review
+                                        </h5>
+                                        <button
+                                            type="button"
+                                            className="btn-close"
+                                            data-bs-dismiss="modal"
+                                            aria-label="Close"
+                                        ></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <StarRatings
+                                            rating={rating}
+                                            starRatedColor="#ffb829"
+                                            numberOfStars={5}
+                                            name="rating"
+                                            changeRating={(e)=>setRating(e)}
+                                        />
 
-                  <button
-                    id="new_review_btn"
-                    className="btn w-100 my-4 px-4"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    Submit
-                  </button>
+                                        <textarea
+                                            name="review"
+                                            id="review"
+                                            className="form-control mt-4"
+                                            placeholder="Enter your comment"
+                                            value={comment}
+                                            onChange={(e)=>setComment(e.target.value)}
+                                        ></textarea>
+
+                                        <button
+                                            id="new_review_btn"
+                                            className="btn w-100 my-4 px-4"
+                                            data-bs-dismiss="modal"
+                                            aria-label="Close"
+                                            onClick={submitHandler}
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default NewReview
