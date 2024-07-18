8047004397a653b03434b070e201cbf43906c75e
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-18 23:46:32 +0530

    Routes segregated as user and admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,14 @@
 import "./App.css"
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
+import { BrowserRouter as Router, Routes} from "react-router-dom"
 import Header from "./components/layout/Header";
-import Home from "./components/Home";
+
 import Footer from "./components/layout/Footer";
 import { Toaster } from 'react-hot-toast';
-import ProductDetails from "./components/product/ProductDetails";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Profile from "./components/user/Profile";
-import UpdateProfile from "./components/user/UpdateProfile";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UploadAvatar from "./components/user/UploadAvatar";
-import UpdatePassword from "./components/user/UpdatePassword";
-import ForgotPassword from "./components/auth/ForgotPassword";
-import ResetPassword from "./components/auth/ResetPassword";
-import Cart from "./components/cart/Cart";
-import Shipping from "./components/cart/Shipping";
-import ConfirmOrder from "./components/cart/ConfirmOrder";
-import PaymentMethod from "./components/cart/PaymentMethod";
-import MyOrders from "./components/order/MyOrders";
-import OrderDetails from "./components/order/OrderDetails";
-import Invoice from "./components/invoice/Invoice";
-
+import useUserRoutes from "./components/routes/userRoutes";
 
 function App() {
+
+  const userRoutes=useUserRoutes()
   return (
     <Router>
       <div className="App">
@@ -34,71 +17,7 @@ function App() {
 
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/password/forgot" element={<ForgotPassword />} />
-            <Route path="/password/reset/:token" element={<ResetPassword />} />
-
-            <Route
-              path="/me/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              } />
-
-            <Route path="/me/update_profile" element={
-              <ProtectedRoute>
-                <UpdateProfile />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/me/upload_avatar" element={
-              <ProtectedRoute>
-                <UploadAvatar />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/me/update_password" element={
-              <ProtectedRoute>
-                <UpdatePassword />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/cart" element={<Cart />} />
-
-            <Route path="/shipping" element={
-              <ProtectedRoute>
-                <Shipping />
-              </ProtectedRoute>} />
-
-            <Route path="/confirm_order" element={
-              <ProtectedRoute>
-                <ConfirmOrder />
-              </ProtectedRoute>} />
-
-              <Route path="/payment_method" element={
-              <ProtectedRoute>
-                <PaymentMethod />
-              </ProtectedRoute>} />
-
-              <Route path="/me/orders" element={
-              <ProtectedRoute>
-                <MyOrders/>
-              </ProtectedRoute>} />
-
-              <Route path="/me/order/:id" element={
-              <ProtectedRoute>
-                <OrderDetails/>
-              </ProtectedRoute>} />
-
-              <Route path="/invoice/order/:id" element={
-              <ProtectedRoute>
-                <Invoice/>
-              </ProtectedRoute>} />
-
+            {userRoutes}
           </Routes>
         </div>
 
