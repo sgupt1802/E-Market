0256e06aec76fb7a44ff263eb6131c7934f1aedb
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-07-18 23:53:09 +0530

    Starting with the Admin

--- frontend/src/components/routes/adminRoutes.jsx
+++ frontend/src/components/routes/adminRoutes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import {Route } from "react-router-dom"
+import ProtectedRoute from "../auth/ProtectedRoute";
+import Dashboard from '../admin/Dashboard';
 const adminRoutes = () => {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <>
+            <Route path="/admin/dashboard" element={
+                <ProtectedRoute>
+                    <Dashboard/>
+                </ProtectedRoute>
+            } />
+
+        </>
+    )
 }
 
 export default adminRoutes
